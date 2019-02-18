dff75a100c6ccf2ee025c33f8842212ca5de75fb
webwyb <beijingwyb@163.com>
2019-02-18 11:27:32 +0800

    feat: tab

---
+++ lib/page/home_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('商城首页'),
+      ),
+    );
+  }
+}
+
+//class HomePage extends StatelessWidget {
+//  @override
+//  Widget build(BuildContext context){
+//    return Scaffold(
+//      body: Center(
+//        child: Text('百姓生活+'),
+//      )
+//    )
+//  }
+//}
