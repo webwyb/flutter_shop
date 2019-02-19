d3f0d0929a9ccf6fb5e50216eb75058a3b556f67
webwyb <beijingwyb@163.com>
2019-02-19 18:34:42 +0800

    Dio 网络请求

--- lib/page/home_page.dart
+++ lib/page/home_page.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:dio/dio.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    getHttp();
     return Scaffold(
       body: Center(
         child: Text('商城首页'),
@@ -11,6 +13,18 @@ class HomePage extends StatelessWidget {
   }
 }
 
+void getHttp() async {
+  try {
+    Response response;
+    var data = {'name': 'jishupang'};
+    response = await Dio().get(
+      "https://www.easy-mock.com/mock/5c60131a4bed3a6342711498/baixing/dabaojian",queryParameters: data
+    );
+    return print(response);
+  } catch (e) {
+    return print(e);
+  }
+}
 //class HomePage extends StatelessWidget {
 //  @override
 //  Widget build(BuildContext context){
